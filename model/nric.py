6d0b04c41ff9f5fa6cbeab4f2aa55d040ab87df3
Jerry U <jerry@lyshnia.com>
2019-04-10 14:40:53 +0100

    cleanup

---
+++ model/nric.py
@@ -0,0 +1,17 @@
+# coding=utf8
+# the above tag defines encoding for this document and is for Python 2.x compatibility
+
+import re
+
+regex = (r'''(?P<nric>\d{6}-\d{2}-\d{4})''')
+
+test_str = ('''--text--''')
+
+matches = re.finditer(regex, test_str, re.MULTILINE)
+data = ""
+for matchNum, match in enumerate(matches):
+    matchNum = matchNum + 1
+    print("{0}".format(match.group('nric')))
+
+
+# Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
