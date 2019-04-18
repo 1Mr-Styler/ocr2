616e423c31f03cf55cfed2f174a816a7a1410300
Jerry U <jerry@lyshnia.com>
2019-04-19 05:01:24 +0800

    Added WUI

---
+++ model/ex.py
@@ -0,0 +1,17 @@
+# coding=utf8
+# the above tag defines encoding for this document and is for Python 2.x compatibility
+
+import re
+
+regex = (r'''.*([Nn]ame[\s\t:;]+?([A-Z]{2,}\s[A-Z]{2,}\s?[A-Z]{2,}?))[\r\n\t]''')
+
+test_str = ('''--text--''')
+
+matches = re.finditer(regex, test_str, re.MULTILINE)
+data = ""
+for matchNum, match in enumerate(matches):
+    matchNum = matchNum + 1
+    print match.group(2).strip()
+
+
+# Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
